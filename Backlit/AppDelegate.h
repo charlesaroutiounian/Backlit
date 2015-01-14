0d00321301e6d4e124d6489ae519fcc6bdf5eb0f
charlesaroutiounian <sierpinski>
2015-01-14 14:36:11 +0100

    Initial Commit

---
+++ Backlit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Backlit
+//
+//  Created by Charles Aroutiounian on 14/01/2015.
+//  Copyright (c) 2015 Charles Aroutiounian. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Backlit/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Backlit
+//
+//  Created by Charles Aroutiounian on 14/01/2015.
+//  Copyright (c) 2015 Charles Aroutiounian. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
